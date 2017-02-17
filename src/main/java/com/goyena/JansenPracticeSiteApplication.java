17c6e70f9ace855c6dcd47f47ee88d20e93a860c
jansen <jansen.goyena@yahoo.com>
2017-02-17 12:33:51 +0800

    Changing to Maven using Spring Tool Suite

---
+++ src/main/java/com/goyena/JansenPracticeSiteApplication.java
@@ -0,0 +1,12 @@
+package com.goyena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JansenPracticeSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JansenPracticeSiteApplication.class, args);
+	}
+}
